2a5aee96dc1aeff5013ae70460aa81e460117259
Alexander Kurz <alex.kurz95@gmail.com>
2025-07-21 12:30:39 +0200

    Set default window size

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, glib};
-
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+use gtk::{Application, ApplicationWindow, Button, glib};
+const APP_ID: &str = "org.gtk_rs.HelloWorld3";
 
 fn main() -> glib::ExitCode {
     // Create a new application
@@ -15,10 +14,32 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    // Create a window and set the title
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Open file")
+        .width_request(100) // Make the button small
+        .height_request(40)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(|button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+
+    // Center the button using a vertical Box
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    vbox.set_valign(gtk::Align::Center);
+    vbox.set_halign(gtk::Align::Center);
+    vbox.append(&button);
+
+    // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("My GTK App")
+        .title("Image Viewer")
+        .default_width(600)
+        .default_height(400)
+        .child(&vbox)
         .build();
 
     // Present window
